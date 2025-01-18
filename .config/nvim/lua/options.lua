37db0ab4a47bcba5b8a6d29264d29d2b170a953c
lare354 <lare354@duck.com>
2025-01-19 00:40:21 +0100

    add files

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,7 @@
+require "nvchad.options"
+
+-- add yours here!
+vim.g.neovide_transparency = 1
+vim.g.neovide_normal_opacity = 0.6
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
