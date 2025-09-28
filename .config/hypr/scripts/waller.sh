18764c16c90bae23072ecc1861a75d2b3410ea1f
lare354 <lare354@duck.com>
2025-09-28 09:59:45 +0200

    waller !

---
+++ .config/hypr/scripts/waller.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+
+#walls dir
+wallsDir="/usr/share/wallpapers/walls-catppuccin-mocha/"
+rofiCmd="rofi -dmenu -theme ~/.config/rofi/waller.rasi"
+
+# Select a picture with rofi
+wallSelection=$(for wall in "$wallsDir"*; do echo -en "$wall\0icon\x1fthumbnail://$wall\n" ; done | $rofiCmd)
+
+# Set the wallpaper
+[[ -n "$wallSelection" ]] || exit 1
+
+swww img ${wallSelection}
+
+exit 0
+
