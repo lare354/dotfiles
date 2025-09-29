5eaef264bb55787e4a09d412644ae191a38455b5
lare354 <lare354@duck.com>
2025-09-29 08:18:02 +0200

    B)

--- .config/hypr/scripts/waller.sh
+++ .config/hypr/scripts/waller.sh
@@ -11,8 +11,25 @@
 
 #wallpaper dir
 wallsDir="/usr/share/wallpapers/walls-catppuccin-mocha/"
-
 rofiCmd="rofi -dmenu -theme ~/.config/rofi/waller.rasi"
+
+cacheDir=$HOME/.cache/waller
+confDir=$HOME/.config/waller
+conf=$confDir/waller.conf
+
+if [ ! -d "$cacheDir" ]; then
+	mkdir $cacheDir	
+fi
+
+if [ ! -d "$conf" ]; then
+	if [ ! -d "$confDir" ]; then
+		mkdir $confDir
+	fi 
+	echo blur=False > $confDir/waller.conf
+fi
+
+. $confDir/waller.conf
+
 
 # Select a picture with rofi
 wallSelection=$(for wall in "$wallsDir"*; do echo -en "$wall\0icon\x1fthumbnail://$wall\n" ; done | $rofiCmd)
@@ -20,7 +37,15 @@ wallSelection=$(for wall in "$wallsDir"*; do echo -en "$wall\0icon\x1fthumbnail:
 # Set the wallpaper
 [[ -n "$wallSelection" ]] || exit 1
 
-swww img ${wallSelection}
+if [ ! $blur ]; then
+	swww img ${wallSelection}
+	exit 0	
+fi
+
+wallName=$(basename "${wallSelection}")
+
+magick ${wallSelection} -scale 50% -blur 0x4 -resize 200% "${cacheDir}/blurred_${wallName}" 
+
+swww img "${cacheDir}/blurred_${wallName}"
 
 exit 0
-
